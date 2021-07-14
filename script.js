cfd3b1718df49a5f69fd4c682a4e63fdb2f17ecb
Nicolás Florentín <nicogrunger@hotmail.com>
2021-07-13 22:20:51 -0300

    Agregar sistema de guardado en local

--- script.js
+++ script.js
@@ -1,35 +1,57 @@
 console.log("script OK");
 
+//////////////////////////////
+// DECLARACION DE VARIABLES //
+//////////////////////////////
+
 var instance = 1;
 var state = 10;
-var playerName = "";
+var playerName = playerName = localStorage.getItem("playerName");
 
-// FUNCION PARA GUARDAR EL NOMBRE, Y OCULATAR EL INPUT
-function clickName() {
-    console.log('Button OK');
-    let name = document.getElementById("nameForm").value;
+//////////////////////////////
+// DECLARACION DE FUNCIONES //
+//////////////////////////////
 
-    // ELIMINAR FORMULARIO
-    let item = document.getElementById("nameDiv");
-    item.parentNode.removeChild(item);
-    
-    // AGREGAR NAME AL DOM
-    let headContainer = document.getElementById("headBar");
-    let paragraph = document.createElement("P");
-    let text = document.createTextNode(name);
-    paragraph.appendChild(text);
-    headContainer.appendChild(paragraph);
+// FUNCION ELIMINAR FORMULARIO
+function eraseForm() {
 
-    // GUARDAR NOMBRE EN VARIABLE GLOBAL
-    playerName = name;
+    // eliminar formulario
+    let formDiv = document.getElementById("nameDiv");
+    formDiv.parentNode.removeChild(formDiv);
+    // agregar name al dom
+    let headDiv = document.getElementById("headBar");
+    let p = document.createElement("P");
+    p.innerHTML = playerName;
+    headDiv.appendChild(p);
+}
 
-}; 
+// FUNCION GUARDAR EL NOMBRE, MOSTRAR NOMBRE Y OCULTAR INPUT
+function storeName() {
+
+    // recuperar nombre del input
+    let nameValue = document.getElementById("nameForm").value;
+    // guardar nombre en variable global y en storage
+    playerName = nameValue;
+    localStorage.setItem('playerName', playerName);
+
+    eraseForm();
+}
+
+// FUNCION BOTON NUEVO JUEGO
+function newGame(){
+
+    // resetea las variables
+    localStorage.setItem('instance', 1);
+    localStorage.setItem('state', 10);
+    localStorage.setItem('playerName', "");
+}
+
+console.log(typeof("playerName"));
 
 // FUNCION QUE DETERMINA QUE HACE CADA BOTON
 function buttonPress(buttonSelect) {
 
-    if (instance <= 10) {
-
+    if (instance <= 10 && playerName != "") {
         let dialog1Show = document.getElementById("dialog1Show");
         let dialog2Show = document.getElementById("dialog2Show");
         let dialog3Show = document.getElementById("dialog3Show");
@@ -89,9 +111,29 @@ function buttonPress(buttonSelect) {
                 web[instance - 1].botonA.dialogStateChange();
                 statisticsShow.innerHTML = state;
                 break;
+
+            case "new":
+                // tratar de recuperar los datos guardados e imprimir dialogos
         }
+
         // pasa a la siguiente instancia
         instance++;
+        // guarda instancia y state en local
+        localStorage.setItem("instance", instance);
+        localStorage.setItem("state", state)
     }
 
 };
+
+
+/////////////////////////////////
+// APENAS SE EJECUTA EL CÓDIGO //
+/////////////////////////////////
+
+// VERIFICAR SI HAY UN NOMBRE GUARDADO, RECUPERAR DATOS Y BORRAR FORMULARIO
+if (playerName != "") {
+
+    instance = parseInt(localStorage.getItem("instance"));
+    state = parseInt(localStorage.getItem("state"));
+    eraseForm();
+}
